ecd91c53ee7bcf379df0e0662e7c6f0bb21460a9
Anthony R. Spyker <anthonyspyker@gmail.com>
2021-04-02 15:49:35 -0400

    added the entire scheme for the weightsModels.js

--- models/weightsModels.js
+++ models/weightsModels.js
@@ -3,8 +3,44 @@ const mongoose = reguire("mongoose")
 const Schema = mongoose.Schema;
  
 const WeightsSchema = new Schema({
-
-})
+    day: {
+        type: Date,
+        default: Date.now
+    },
+    exercises: [
+        {type:{
+            type: String,
+            trim: true,
+            required: true
+        },
+        name: {
+            type: String,
+            trim: true,
+            required: true
+        },
+        duration: {
+            type: Number,
+            required: true
+        },
+        weight: {
+            type: Number
+        },
+        reps: {
+            type: Number
+        },
+        sets: {
+            type: Number
+        },
+        distance: {
+            type: Number
+        }
+    }]
+},
+{
+    toJSON: {
+        virtuals: true
+    }
+});
 
 const Weights = mongoose.models("Weights", WeightsSchema);
 
