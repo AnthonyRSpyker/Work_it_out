054bd2bbf6982547dc32dd53b1081f02f4073293
Anthony R. Spyker <anthonyspyker@gmail.com>
2021-04-03 09:33:01 -0400

    added api routes. One to get the ranges, one to update the excercisesand one to find all workouts.

--- server.js
+++ server.js
@@ -23,16 +23,60 @@ mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { use
 
 //api routes
 app.post("/api/workouts", (req, res) => {
-db.Workout.create({})
-    .then(dbWorkout => {
-        res.json(dbWorkout)
-        console.log(dbWorkout);
-    })
-    .catch(err => {
-        console.log(err)
+    db.Workout.create({})
+        .then(dbWorkout => {
+            res.json(dbWorkout)
+            console.log(dbWorkout);
+        })
+        .catch(err => {
+            console.log(err)
+        });
     });
-});
+    
+    app.get("/api/workouts", (req, res) => {
+        db.Workout.find()
+            .then(dbWorkout => {
+                res.json(dbWorkout)
+            })
+            .catch(err => {
+                res.json(err)
+            })
+    })
+    
+    app.put("/api/workouts/:id", (req, res) => {
+        db.Workout.findByIdAndUpdate(
+            req.params.id,
+            {
+                $push: {
+                    exercises: req.body
+                }
+            },
+            {
+                new: true,
+                runValidators: true
+            }
+            )
+            .then(dbWorkout => {
+                res.json(dbWorkout)
+            })
+            .catch(err => {
+                res.json(err)
+            })
+            
+    });
+    
+    app.get("/api/workouts/range", (req, res) => {
+        db.Workout.find().limit(7)
+            .then(dbWorkout => {
+                res.json(dbWorkout)
+            })
+            .catch(err => {
+                res.json(err)
+            })
+    });
+    
 
+app.getMaxListeners("/api/workouts",)
 //html routes
 
 app.get("/exercise", (req, res) => {
